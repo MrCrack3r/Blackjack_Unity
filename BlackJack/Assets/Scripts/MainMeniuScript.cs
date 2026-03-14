3ef278d32e6717f868f5ce499ed4dfdb85fcdbf1
Simpelgud <racickasnedas@gmail.com>
2026-03-14 17:10:53 +0200

    Padarytos likusios logikos
    Padarytos end screen ir grįžimo į main meniu logikos.

---
+++ BlackJack/Assets/Scripts/MainMeniuScript.cs
@@ -0,0 +1,19 @@
+﻿// MainMenuManager.cs
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Funkcija žaidimo pradžiai
+    public void StartGame()
+    {
+        // Naudojame scenos pavadinimą iš jūsų projekto struktūros
+        SceneManager.LoadScene("Blackjack_table_scene");
+    }
+
+    // Funkcija išėjimui iš žaidimo
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ BlackJack/Assets/Scripts/MeniuButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // BŪTINA scenų keitimui!
+
+public class GameMenuController : MonoBehaviour
+{
+    // Ši funkcija grąžins mus į pagrindinį meniu
+    public void GoToMainMenu()
+    {
+        Debug.Log("Grįžtama į pagrindinį meniu...");
+        // Įrašykite TIKSLŲ savo pagrindinio meniu scenos pavadinimą
+        SceneManager.LoadScene("Main_menu_scene");
+    }
+}
