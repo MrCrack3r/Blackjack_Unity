e79ff3cb25361ecde86a5133132c313988bffe19
Pociuss <domantas.pocius13@gmail.com>
2026-04-29 19:53:59 +0300

    Įdiegta End Screen statistikos išsaugojimo ir atvaizdavimo sistema
    Pridėta detalios žaidimo statistikos išsaugojimo ir rodymo funkcionalumas End Screen lange.
    
    Įgyvendinta:
    - Išsaugoma paskutinio žaidimo statistika pralaimėjus
    - Sekamas didžiausias šio run metu turėtas biudžetas
    - Sekamas nupirktų power-up kiekis per run
    - Išsaugomi visų laikų geriausi rezultatai (raundas ir biudžetas)
    - Statistika atvaizduojama End Screen naudojant TextMeshPro UI
    - Po Game Over pašalinamas aktyvaus run save, išsaugant statistiką
    
    Tai leidžia žaidėjui matyti tiek paskutinio žaidimo rezultatus,
    tiek visų laikų geriausius asmeninius rekordus.

---
+++ BlackJack/Assets/Scripts/EndScreenStats.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using TMPro;
+
+
+public class EndScreenStats : MonoBehaviour
+{
+	public TMP_Text lastRunRoundText;
+	public TMP_Text lastRunMoneyText;
+	public TMP_Text lastRunGamesText;
+	public TMP_Text lastRunPowerUpsText;
+
+	public TMP_Text bestRoundText;
+	public TMP_Text bestMoneyText;
+
+	void Start()
+	{
+		lastRunRoundText.text = "Pasiektas raundas: " + PlayerPrefs.GetInt("LastRunRound", 0);
+		lastRunMoneyText.text = "Likęs biudžetas: $" + PlayerPrefs.GetInt("LastRunMoney", 0);
+		lastRunGamesText.text = "Sužaista partijų: " + PlayerPrefs.GetInt("LastRunGames", 0);
+		lastRunPowerUpsText.text = "Nupirkta galių: " + PlayerPrefs.GetInt("LastRunPowerUps", 0);
+
+		bestRoundText.text = "Geriausias raundas: " + PlayerPrefs.GetInt("BestRound", 0);
+		bestMoneyText.text = "Didžiausias biudžetas: $" + PlayerPrefs.GetInt("BestMoney", 0);
+	}
+}
