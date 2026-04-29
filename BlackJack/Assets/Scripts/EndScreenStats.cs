4b1d53fa42330f8e3c31d07449957f40367a495f
Pociuss <domantas.pocius13@gmail.com>
2026-04-29 21:03:15 +0300

    Įgyvendinta detalios žaidimo statistikos ir rekordų sekimo logika, papildyta
    Sukurta sistema, kuri žaidimo metu automatiškai renka ir išsaugo papildomą run statistiką bei rekordus.
    
    Įgyvendinta:
    
    Sekamas laimėtų handų skaičius
    Sekamas pralaimėtų handų skaičius
    Sekamas sunaudotų kortų kiekis per run
    Statistika išsaugoma žaidimui pasibaigus naudojant PlayerPrefs
    Rekordai atnaujinami tik pasiekus geresnį rezultatą nei ankstesnis
    Visa statistika atvaizduojama End Screen lange
    
    Papildomai:
    
    Išplėstas RunManager statistikos sekimas
    Papildytas EndScreenStats duomenų nuskaitymas ir pateikimas UI
    Užtikrintas korektiškas statistikos persidavimas tarp scenų

--- BlackJack/Assets/Scripts/EndScreenStats.cs
+++ BlackJack/Assets/Scripts/EndScreenStats.cs
@@ -8,6 +8,9 @@ public class EndScreenStats : MonoBehaviour
 	public TMP_Text lastRunMoneyText;
 	public TMP_Text lastRunGamesText;
 	public TMP_Text lastRunPowerUpsText;
+	public TMP_Text lastRunHandsWonText;
+	public TMP_Text lastRunHandsLostText;
+	public TMP_Text lastRunCardsUsedText;
 
 	public TMP_Text bestRoundText;
 	public TMP_Text bestMoneyText;
@@ -18,6 +21,9 @@ public class EndScreenStats : MonoBehaviour
 		lastRunMoneyText.text = "Likęs biudžetas: $" + PlayerPrefs.GetInt("LastRunMoney", 0);
 		lastRunGamesText.text = "Sužaista partijų: " + PlayerPrefs.GetInt("LastRunGames", 0);
 		lastRunPowerUpsText.text = "Nupirkta galių: " + PlayerPrefs.GetInt("LastRunPowerUps", 0);
+		lastRunHandsWonText.text = "Laimėta handų: " + PlayerPrefs.GetInt("LastRunHandsWon", 0);
+		lastRunHandsLostText.text = "Pralaimėta handų: " + PlayerPrefs.GetInt("LastRunHandsLost", 0);
+		lastRunCardsUsedText.text = "Sunaudota kortų: " + PlayerPrefs.GetInt("LastRunCardsUsed", 0);
 
 		bestRoundText.text = "Geriausias raundas: " + PlayerPrefs.GetInt("BestRound", 0);
 		bestMoneyText.text = "Didžiausias biudžetas: $" + PlayerPrefs.GetInt("BestMoney", 0);
